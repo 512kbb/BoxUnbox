d66f6e5fe27d66a37b8a3667a9b93cca2873626c
512kbb <dieg.parraa@duocuc.cl>
2023-10-11 14:26:29 -0300

    first commit'

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BoxUnbox;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Object> list = new List<object> { 7, 28, -1, true, "chair" };
+
+        int accumulador = 0;
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+            if (item is int)
+            {
+                accumulador += (int)item;
+            }
+        }
+        Console.WriteLine($"resultado: {accumulador}");
+
+    }
+}
